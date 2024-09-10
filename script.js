f285b55556ef999fb019770479ca72f7d5fd973c
Eagler <jayden.okubadej@outlook.com>
2024-09-09 20:04:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function showTab(tabName) {
     var tabs = document.getElementsByClassName("tab");
     for (var i = 0; i < tabs.length; i++) {
-        tabs[i].style.display = "none";
+        tabs[i].style.opacity = 0; // Fade out current tabs
+        setTimeout(function(tab) {
+            tab.style.display = "none"; // Hide after fade out
+        }, 300, tabs[i]);
     }
-    document.getElementById(tabName).style.display = "block";
+    var currentTab = document.getElementById(tabName);
+    currentTab.style.display = "block"; // Show the selected tab
+    setTimeout(function() {
+        currentTab.style.opacity = 1; // Fade in the selected tab
+    }, 10);
 }
 
 function updatePlayButton() {
